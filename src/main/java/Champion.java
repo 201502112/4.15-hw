cf49982df65f367ac633516f3697f247e94c2e2d
201502112 <gkh03209@naver.com>
2019-04-21 18:33:25 +0900

    study at school

--- src/main/java/Champion.java
+++ src/main/java/Champion.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@AllArgsConstructor
+@Data //lombok이 getter/setter 자동 생성해줌
 public class Champion {
+    private String name;
+    private String position;
 }
